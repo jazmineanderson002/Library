b8bef2dc30f42701ef445b32e3684506200ad8c1
Jazmine Anderson <jazmine.anderson2002@gmail.com>
2019-08-27 08:26:45 -0700

    Update Fiction.java

--- src/Fiction.java
+++ src/Fiction.java
@@ -1,4 +1,36 @@
 public class Fiction extends Book
 {
+  String mainCharacter;
 
+  public Fiction(int numPages, String title, String author, boolean isAssigned, String genre, String mainCharacter)
+  {
+    super(numPages, title, author, isAssigned, genre);
+    this.mainCharacter = mainCharacter;
+  } // end full constructor with super
+
+  //getters
+
+  public String getMainCharacter()
+  {
+    return mainCharacter;
+  }
+
+  //setters
+
+  public void setMainCharacter(String mainCharacter)
+  {
+    this.mainCharacter = mainCharacter;
+  }
+
+  @Override
+  public void CheckOut(int studentId)
+  {
+    super.CheckOut(studentId);
+  }
+
+  @Override
+  public String toString()
+  {
+    return "Fiction: " +  title + " by " + author + " has " + numPages + " pages. " + "It is " + isAssigned +" that it is assigned and its genre is " + genre + ". The main character is " + mainCharacter ;
+  }
 }
